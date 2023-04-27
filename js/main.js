af0b0190dea66f84eb77342f500ea31b48f86243
DsnQq <58346681+daniil45korzich@users.noreply.github.com>
2023-04-27 18:07:51 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    $('.sliders-block-wrap').slick({
+        dots: false,
+        arrows: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        prevArrow: '<div class="prev"></div>',
+        nextArrow: '<div class="next"></div>',
+        responsive: [
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+        ]
+    });
+
+    $('.close-icons').on('click', function () {
+        $('.modal').fadeOut();
+    });
+
+    $('.modal__overlay').on('click', function () {
+        $('.modal').fadeOut();
+    });
+
+    $('.header-middle-call').on('click', function () {
+        $('.modal').fadeIn();
+    });
+
+    $('.burger-mobil').on('click', function () {
+        $('.header-menu').fadeIn();
+    });
+
+    $('.close-icons-m').on('click', function () {
+        $('.header-menu').fadeOut();
+    });
+
+});
